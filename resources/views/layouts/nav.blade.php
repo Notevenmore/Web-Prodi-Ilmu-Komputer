4a497081410a86847791043de020a5e6ba148a3e
GOLDENWISE <abdamadhafiz13@gmail.com>
2024-06-02 01:39:38 +0700

    Membuat versi responsif web

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -3,7 +3,7 @@
       <div class="py-4">
           <img src="{{ asset("/img/logo-navbar.png") }}" alt="" class="w-72">
       </div>
-      <div class="px-4 text-lg">
+      <div class="px-4 text-lg missing">
           <ul class="flex items-center gap-[2vw]">
               <li><a href="" class="group hover:text-sky-500">Beranda</a></li>
               <li><a href="" class="group hover:text-sky-500">Akademik</a></li>
