1e7fc48f0a058c366708696e67f13a708347c1f1
ziqi-02 <ahmadarroziqi@gmail.com>
2024-07-25 04:48:21 +0700

    make components

---
+++ app/Livewire/Event/Event.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Event;
+
+use Livewire\Component;
+
+class Event extends Component
+{
+    public function render()
+    {
+        return view('livewire.event.event');
+    }
+}
---
+++ resources/views/livewire/partnership/partnership.blade.php
@@ -0,0 +1,8 @@
+<section id="content-partnership">
+  <h1 class="title">Partnership</h1>
+  <div class="partnership-logo">
+    <img src="{{ asset('img/Pertamina.png') }}" alt="">
+    <img src="{{ asset('img/Pertamina Foundation.png') }}" alt="">
+    <img src="{{ asset('img/Universitas Pertamina.png') }}" alt="" width=156 height=106>
+  </div>
+</section>
