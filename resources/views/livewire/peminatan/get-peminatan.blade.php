724b2850a3cb5899964f44ad85050e98af73a374
Notevenmore <hafizsecond635@gmail.com>
2024-07-26 12:45:25 +0700

    Branch Merging

--- resources/views/livewire/peminatan/get-peminatan.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="flex-wrap flex justify-center w-full gap-y-10 text-white">
-  @foreach ($peminatans as $peminatan)
-    <x-peminatan.card :title="$peminatan['title']" :description="$peminatan['description']" :image="$peminatan['image']" />
-  @endforeach
-</div>
